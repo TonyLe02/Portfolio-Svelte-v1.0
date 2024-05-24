235fe82debe9482b3f8eb7c99cd647a97cc7fc7f
Tony Le <111498490+TonyLe02@users.noreply.github.com>
2024-05-24 16:59:09 +0200

    Update About.svelte

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -21,7 +21,7 @@
         <br> I enjoy building applications and am constantly learning new things to improve my skills.
     </p>
     <p class="text-2xl mb-8 text-white">I hope you enjoy scrolling through and learning more about me!</p>
-    <a href="https://cl.gy/XWop"
+    <a href="https://www.linkedin.com/in/tonynl02"
         class="flex justify-center bg-gradient-to-r from-red-500 via-yellow-500 to-blue-500 text-white font-bold py-2 px-4 hover:bg-gradient-to-r hover:from-red-600 hover:via-yellow-600 hover:to-blue-600 rounded transition duration-300 ease-in-out">
         Want to learn more about me?
     </a>
