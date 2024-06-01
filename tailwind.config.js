c6abfc94fcdd8f1f90bb64d469f585dae6cc9932
Tony Le <111498490+TonyLe02@users.noreply.github.com>
2024-06-01 14:32:47 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   content: ['./src/**/*.{js,ts,svelte}', './public/index.html'],
   darkMode: 'media', // or remove this line if you don't want to use dark mode
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        svelte: '#FF3E00',
+      },
+    },
   },
   variants: {
     extend: {},
