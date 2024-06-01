766e00cef6d5e78371d4fa50bdd6b79793328731
Tony Le <111498490+TonyLe02@users.noreply.github.com>
2024-06-01 14:32:45 +0200

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
+// postcss.config.cjs
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer')
-  ]
-}
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
