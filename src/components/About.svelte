48a258e3650b0c0c703be848b56a0a783154ed34
Tony Le <111498490+TonyLe02@users.noreply.github.com>
2024-08-17 16:31:13 +0200

    Update About.svelte styling and remove unnecessary code

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -29,32 +29,28 @@
     class="rounded-full w-32 h-32 mx-auto mb-4"
     aria-hidden="true"
   />
-  <h1 class="block font-extrabold text-4xl py-2 text-white mb-2">
+  <h1
+    class="block font-extrabold text-4xl py-2 bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-blue-600 to-red-500 mb-2"
+  >
     Tony Nguyen Le
   </h1>
   <p
-    class="text-xl font-extrabold text-white mb-8"
+    class="text-xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-blue-600 to-red-500 mb-8"
   >
     I'm an aspiring developer with a passion for data technology.
   </p>
 
-  <p
-    class="text-2xl mb-8 bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-blue-600 to-red-500"
-  >
+  <p class="text-2xl mb-8 text-white">
     Currently in my 3rd year of IT & Information Systems at the University of
     Agder.
   </p>
 
-  <p
-    class="text-2xl mb-8 bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-blue-600 to-red-500"
-  >
+  <p class="text-2xl mb-8 text-white">
     I enjoy building applications and am constantly learning new things to
     improve my skills.
   </p>
 
-  <p
-    class="text-2xl mb-8 bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 via-blue-600 to-red-500"
-  >
+  <p class="text-2xl mb-8 text-white">
     I hope you enjoy scrolling through and learning more about me!
   </p>
 </section>
