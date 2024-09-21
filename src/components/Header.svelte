fa923e5c07fa70afa2b61c9917c9d2359eeedc7c
Tony Nguyen Le <111498490+TonyLe02@users.noreply.github.com>
2024-09-21 11:59:01 +0200

    Refactor Header.svelte and update logo and title

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -45,9 +45,18 @@
 
 <!-- Flowbite Navbar -->
 <nav class="bg-white shadow border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+  <div
+    class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+  >
     <a href="./index.html" class="flex items-center">
-      <span class="self-center text-2xl text-gray-800 font-semibold whitespace-nowrap dark:text-white">
+      <img
+        src="static/assets/favicon.png"
+        alt="LeWorks Logo"
+        class="h-6 mr-2 sm:h-9"
+      />
+      <span
+        class="self-center text-2xl text-gray-800 font-semibold whitespace-nowrap dark:text-white"
+      >
         LeWorks
       </span>
     </a>
